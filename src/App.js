d1622977abdf16bf66a6e1aa0bc19e8904ac34c4
syahrul-25 <pas25kata@gmail.com>
2021-09-15 17:50:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, {useEffect, useState} from "react"
+import {Button, FormControl, InputLabel, Input} from "@material-ui/core"
+import Message from "./Message"
+import db from "./config"
+import firebase from "firebase"
+import FlipMove from "react-flip-move"
+import {Send} from "@material-ui/icons"
+import {IconButton} from "@material-ui/core"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState("")
+	const [messages, setMessages] = useState([])
+	const [username, setUsername] = useState("")
+
+	useEffect(() => {
+		db.collection("messages")
+			.orderBy("timestamp", "desc")
+			.onSnapshot(snapshot => {
+				setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
+			})
+	}, [])
+
+	useEffect(() => {
+		setUsername(prompt("please enter your name"))
+	}, [])
+
+	const sendMessage = e => {
+		e.preventDefault()
+
+		db.collection("messages").add({
+			message: input,
+			username,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp()
+		})
+		setMessages([...messages, {username, message: input}])
+		setInput("")
+	}
+
+	return (
+		<div className="App">
+			<img src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=100&h=100" alt="messenger-logo" />
+			<h1>Hello World</h1>
+			<h2>Welcome {username}</h2>
+
+			<form onSubmit={sendMessage} className="app__form">
+				<FormControl className="app__formControl">
+					<Input className="app__input" placeholder="Enter a message..." id="my-input" value={input} onChange={e => setInput(e.target.value)} />
+					<IconButton disabled={!input} type="submit" onClick={sendMessage} color="primary" variant="outline" className="app__iconButton">
+						<Send />
+					</IconButton>
+				</FormControl>
+			</form>
+
+			<FlipMove>
+				{messages.map(({id, message}) => (
+					<Message username={username} message={message} key={id} />
+				))}
+			</FlipMove>
+		</div>
+	)
 }
 
-export default App;
+export default App
