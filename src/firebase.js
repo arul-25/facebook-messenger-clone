a617497f36468313334f7a4aa27a11486d5e78ad
arul-25 <arulprojectdualima@gmail.com>
2021-09-17 20:33:10 +0900

    edit setup config in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase"
 firebase.initializeApp({
 	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-	apiKey: "AIzaSyBLGXeBG5rGZAfKQRkmu56sbYsRFUrCHeM",
-	authDomain: "facebook-messenger-clone-2c2ce.firebaseapp.com",
-	databaseURL: "https://facebook-messenger-clone-2c2ce-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "facebook-messenger-clone-2c2ce",
-	storageBucket: "facebook-messenger-clone-2c2ce.appspot.com",
-	messagingSenderId: "87247765099",
-	appId: "1:87247765099:web:b2e6290f54f9b890dabb2e",
-	measurementId: "G-3CKLBCQY3V"
+	apiKey: "AIzaSyCDAX7Akv8y98SjR--Vr_RduuMTh_hhIAE",
+	authDomain: "react-messenger-clone-df4d0.firebaseapp.com",
+	projectId: "react-messenger-clone-df4d0",
+	storageBucket: "react-messenger-clone-df4d0.appspot.com",
+	messagingSenderId: "627191931226",
+	appId: "1:627191931226:web:74639c8496e94c1be65363",
+	measurementId: "G-GX81M4DRD0"
 })
 
 const db = firebase.firestore()
